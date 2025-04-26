efdec78821d0f111e17beb2f3110be4dd7e77acc
Mark Sobkow <mark.sobkow@gmail.com>
2025-04-26 09:25:29 -0600

    Massive commit of all of todays work including an overnight stint that has seen me up since 3PM yesterday - it is now 9:20 AM 2025-04-26.

--- ManufactureCFAcc31Java.bash
+++ ManufactureCFAcc31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin
-./CFManufactureCode213Java.bash ${buildString} com-github-msobkow-CFAcc-3-1-CFAcc org-msscf-msscf-213-layered+msscf ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} org-msscf-msscf-CFAcc-3-1-CFAcc org-msscf-msscf-213-layered+msscf ${toolsetNames}
 cd $MSSCFHOME
