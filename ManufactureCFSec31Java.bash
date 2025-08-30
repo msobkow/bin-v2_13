92a89104974b326f5d86e75d04f6b015934199cf
Mark Sobkow <mark.sobkow@gmail.com>
2025-08-30 14:31:14 -0600

    Tweak the manufacturing to do the MssCF support as well.

--- ManufactureCFSec31Java.bash
+++ ManufactureCFSec31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin
-./CFManufactureCode213Java.bash ${buildString} org-msscf-msscf-CFSec-3-1-CFSec org-msscf-msscf-213-layered ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} org-msscf-msscf-CFSec-3-1-CFSec org-msscf-msscf-213-layered+msscf ${toolsetNames}
 cd $MSSCFHOME
