dd5841bac33c152be19d2b01492ed020c3876201
Mark Sobkow <mark.sobkow@gmail.com>
2025-08-30 17:17:43 -0600

    End of Saturdays work

--- ManufactureCFInt31Java.bash
+++ ManufactureCFInt31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin
-./CFManufactureCode213Java.bash ${buildString} org-msscf-msscf-CFInt-3-1-CFInt org-msscf-msscf-213-layered+msscf ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} server-markhome-msscf-CFInt-3-1-CFInt org-msscf-msscf-213-layered+msscf ${toolsetNames}
 cd $MSSCFHOME
