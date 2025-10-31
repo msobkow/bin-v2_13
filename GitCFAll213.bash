89dfdd6715a1927fb06565f3a9f49322c841ee74
Mark Sobkow <mark.sobkow@gmail.com>
2025-10-30 18:28:17 -0600

    Work as of 2025-10-30 18h30

--- GitCFAll213.bash
+++ GitCFAll213.bash
@@ -1,27 +1,10 @@
 #!/bin/bash
-cd $MSSCFHOME
-pushd cflib_2_13
-$GIT $*
-popd
-pushd cfcore_2_13
-$GIT $*
-popd
-pushd cfsec_2_13
-$GIT $*
-popd
-pushd cfint_2_13
-$GIT $*
-popd
-pushd cfbam_2_13
-$GIT $*
-popd
-pushd msscf_2_13
-$GIT $*
-popd
-pushd cfkbase_2_13
-$GIT $*
-popd
-pushd cfmodel_2_13
-$GIT $*
-popd
+export cmdargs=$*
+cd $MSSCFHOME/v2_13-maven/
+for adir in bin cfkbase cfmodel org.msscf.msscf.v2_13 org.msscf.msscf.v2_13.cfbam org.msscf.msscf.v2_13.cfbam.CFBamMssCF org.msscf.msscf.v2_13.cfbam.CFBamRam org.msscf.msscf.v2_13.cfbam.CFBamSaxLoader org.msscf.msscf.v2_13.cfbam.CFBamSaxRamLdr org.msscf.msscf.v2_13.cfbamcust.CFBamXmlLoader org.msscf.msscf.v2_13.cfbamcust.MSSBamCF org.msscf.msscf.v2_13.CFCli org.msscf.msscf.v2_13.cfcore org.msscf.msscf.v2_13.cfint org.msscf.msscf.v2_13.cfint.CFIntRam org.msscf.msscf.v2_13.cfint.CFIntSaxLoader org.msscf.msscf.v2_13.cfint.CFIntSaxRamLdr org.msscf.msscf.v2_13.cflib.CFLib org.msscf.msscf.v2_13.cflib.CFLib.DbTest org.msscf.msscf.v2_13.cflib.CFLib.JavaFX org.msscf.msscf.v2_13.cfsec org.msscf.msscf.v2_13.cfsec.CFSecRam org.msscf.msscf.v2_13.cfsec.CFSecSaxLoader org.msscf.msscf.v2_13.cfsec.CFSecSaxRamLdr
+	do
+			pushd $adir
+			git $cmdargs
+			popd
+	done
 cd $MSSCFHOME
--- GitMakeBashExecutableCFAll213.bash
+++ GitMakeBashExecutableCFAll213.bash
@@ -1,38 +1,11 @@
 #!/bin/bash
-cd $MSSCFHOME
-pushd cflib_2_13
-chmod 755 `find . -name '*.bash'`
-$GIT add `find . -name '.gitignore' -o -name '*.bash'`
-$GIT update-index --add --chmod=+x `find . -name '*.bash'`
-popd
-pushd cfcore_2_13
-chmod 755 `find . -name '*.bash'`
-$GIT add `find . -name '.gitignore' -o -name '*.bash'`
-$GIT update-index --add --chmod=+x `find . -name '*.bash'`
-popd
-pushd cfsec_2_13
-chmod 755 `find . -name '*.bash'`
-$GIT add `find . -name '.gitignore' -o -name '*.bash'`
-$GIT update-index --add --chmod=+x `find . -name '*.bash'`
-popd
-pushd cfint_2_13
-chmod 755 `find . -name '*.bash'`
-$GIT add `find . -name '.gitignore' -o -name '*.bash'`
-$GIT update-index --add --chmod=+x `find . -name '*.bash'`
-popd
-pushd cfbam_2_13
-chmod 755 `find . -name '*.bash'`
-$GIT add `find . -name '.gitignore' -o -name '*.bash'`
-$GIT update-index --add --chmod=+x `find . -name '*.bash'`
-popd
-pushd msscf_2_13
-chmod 755 `find . -name '*.bash'`
-$GIT add `find . -name '.gitignore' -o -name '*.bash'`
-$GIT update-index --add --chmod=+x `find . -name '*.bash'`
-popd
-#pushd cfkbase_2_13
-#chmod 755 `find . -name '*.bash'`
-#$GIT add `find . -name '.gitignore' -o -name '*.bash'`
-#$GIT update-index --add --chmod=+x `find . -name '*.bash'`
-#popd
+cd $MSSCFHOME/v2_13-maven/
+for adir in bin cfkbase cfmodel org.msscf.msscf.v2_13 org.msscf.msscf.v2_13.cfbam org.msscf.msscf.v2_13.cfbam.CFBamMssCF org.msscf.msscf.v2_13.cfbam.CFBamRam org.msscf.msscf.v2_13.cfbam.CFBamSaxLoader org.msscf.msscf.v2_13.cfbam.CFBamSaxRamLdr org.msscf.msscf.v2_13.cfbamcust.CFBamXmlLoader org.msscf.msscf.v2_13.cfbamcust.MSSBamCF org.msscf.msscf.v2_13.CFCli org.msscf.msscf.v2_13.cfcore org.msscf.msscf.v2_13.cfint org.msscf.msscf.v2_13.cfint.CFIntRam org.msscf.msscf.v2_13.cfint.CFIntSaxLoader org.msscf.msscf.v2_13.cfint.CFIntSaxRamLdr org.msscf.msscf.v2_13.cflib.CFLib org.msscf.msscf.v2_13.cflib.CFLib.DbTest org.msscf.msscf.v2_13.cflib.CFLib.JavaFX org.msscf.msscf.v2_13.cfsec org.msscf.msscf.v2_13.cfsec.CFSecRam org.msscf.msscf.v2_13.cfsec.CFSecSaxLoader org.msscf.msscf.v2_13.cfsec.CFSecSaxRamLdr
+do
+		pushd $adir
+		chmod 755 `find . -name '*.bash'`
+		git add `find . -name '.gitignore' -o -name '*.bash'`
+		git update-index --add --chmod=+x `find . -name '*.bash'`
+		popd
+done
 cd $MSSCFHOME
