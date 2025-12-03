48b8bfaa7af7bbf6fad41b08ac70a5c6dc49181b
Mark Sobkow <mark.sobkow@gmail.com>
2025-12-03 15:24:13 -0600

    Updated code count

--- CountCFAll31.bash
+++ CountCFAll31.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd $MSSCFHOME
-codecount cflib_3_1 cfcore_3_1 cfsec_3_1 cfint_3_1 cfcrm_3_1 cfdbtst_3_1 cfbam_3_1 msscf_3_1 cfacc_3_1 > htdocs/msobkow.github.com/CodeCount31-`date +%Y%m%d`.txt
+cd $MSSCFHOME/v3_1/java
+mydate=`date +%Y-%m-%d-%Hh%M`
+codecount server.markhome.mcf.v3_1.* | tee $HOME/count-v3_1-${mydate}.txt
