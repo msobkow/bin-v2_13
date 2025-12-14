eb5578ea80e1e864e381b1ed42840ba4cb3aaedc
Mark Sobkow <mark.sobkow@gmail.com>
2025-12-14 04:21:21 -0600

    2025-12-14 04:20 pardon the interruption, but I must...

--- CountCFAll31.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $MSSCFHOME/v3_1/java
-mydate=`date +%Y-%m-%d-%Hh%M`
-codecount server.markhome.mcf.v3_1.* | tee $HOME/count-v3_1-${mydate}.txt
--- GitMakeBashExecutableCFAll31.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for adir in $MSSCFHOME/v3_1-maven/java/* $MSSCFHOME/v3_1/java/*
-do
-		pushd $adir
-		chmod 755 `find . -name '*.bash'`
-		git add `find . -name '.gitignore' -o -name '*.bash'`
-		git update-index --add --chmod=+x `find . -name '*.bash'`
-		popd
-done
