0c4dd5e048023aa90dc1ee32a4a5eac2a3aadb67
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-10 16:25:47 -0600

    Overview -------- The rules defined herein presume a specific combination of tools and releases. Java JDK 17, GWT 2.12.2, and Spring 4.0. GWT is the limiting technology for the supported JDK, but that only applies with GWT code aspects of the overall outputs for the builds. If you do not use nor require GWT, simply don't build those modules nor ship them; you can obviously compile the other code with higher releases of JDK than 17 without changing anything more than your copy of the pom.xml and your package names and hierarchy to reflect the branch.
    As of 2026-01-10 16h15 CST, the output code base and rule base are actually
    JDK 25 compatible and even require JDK 25 for the core interfaces produced.
    Those embedded interface constants will have to move to a hierachy of classes
    with static class members instead, but in the same package. Then the rules need
    to be updated to reference the new classes instead of the interfaces, and we should
    be back on the road with New Code...

--- ManufactureCFDbTst31Java.bash
+++ ManufactureCFDbTst31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin
-./CFManufactureCode213Java.bash ${buildString} com-github-msobkow-CFDbTst-3-1-CFDbTst server-markhome-mcf-30-layered+msscf ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} com-github-msobkow-CFDbTst-3-1-CFDbTst server-markhome-mcf-30-layered+mcf ${toolsetNames}
 cd $MSSCFHOME
--- ManufactureCFInt31Java.bash
+++ ManufactureCFInt31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin
-./CFManufactureCode213Java.bash ${buildString} server-markhome-mcf-CFInt-3-1-CFInt server-markhome-mcf-30-layered+msscf ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} server-markhome-mcf-CFInt-3-1-CFInt server-markhome-mcf-30-layered+mcf ${toolsetNames}
 cd $MSSCFHOME
