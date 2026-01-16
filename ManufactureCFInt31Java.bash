6cb4582c6123b659b601c9c7c61f75e1588f2879
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-16 01:27:48 -0600

    2026-01-16 01:25 Finalized model targets as io.github.msobkow instead of server.markhome.mcf. Next I'll rework my URLs for v2.13 to reference msobkow.github.io instead of msscf.org.

--- ManufactureCFInt31Java.bash
+++ ManufactureCFInt31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin-v2_13
-./CFManufactureCode213Java.bash ${buildString} server-markhome-mcf-CFInt-3-1-CFInt server-markhome-mcf-30-layered+mcf ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} io-github-msobkow-CFInt-3-1-CFInt server-markhome-mcf-30-layered+mcf ${toolsetNames}
 cd $MSSCFHOME
