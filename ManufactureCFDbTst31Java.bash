cfbe216e092c9a98bcbc74afd41f431b696013bd
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-16 05:36:02 -0600

    2026-01-16 05:35 I believe the remapping of packages from server.markhome.mcf to io.github.msobkow is complete.

--- ManufactureCFDbTst31Java.bash
+++ ManufactureCFDbTst31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin-v2_13
-./CFManufactureCode213Java.bash ${buildString} io-github-msobkow-CFDbTst-3-1-CFDbTst server-markhome-mcf-30-layered+mcf ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} io-github-msobkow-CFDbTst-3-1-CFDbTst io-github-msobkow-30-layered+mcf ${toolsetNames}
 cd $MSSCFHOME
--- ManufactureCFSec31Java.bash
+++ ManufactureCFSec31Java.bash
@@ -2,5 +2,5 @@
 buildString=$1
 toolsetNames="$2 $3 $4 $5 $6 $7 $8 $9"
 cd $MSSCFHOME/v2_13-maven/bin-v2_13
-./CFManufactureCode213Java.bash ${buildString} io-github-msobkow-CFSec-3-1-CFSec server-markhome-mcf-30-layered+mcf ${toolsetNames}
+./CFManufactureCode213Java.bash ${buildString} io-github-msobkow-CFSec-3-1-CFSec io-github-msobkow-30-layered+mcf ${toolsetNames}
 cd $MSSCFHOME
